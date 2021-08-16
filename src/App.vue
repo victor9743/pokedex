e9d27e1bb689684bbcc4ad610b3e78fd836cffbe
victor9743 <70709626+victor9743@users.noreply.github.com>
2021-08-16 16:22:02 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="column is-half is-offset-one-quarter">
+
+      <h4 class="is-size-4">Pokedex</h4>
+
+          <input type="text" class="input is-rounded" placeholder="Buscar Pokemon pelo Nome" v-model="busca">
+          <button class="button is-fullwidth is-success" id="busca_btn" @click="buscar">Busca</button>
+
+          <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+
+            <pokemon :num="index +1" :name="poke.name" :url="poke.url"/>
+          </div>
+    </div>
+  
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
+import pokemon from './components/pokemon.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+        pokemons: [],
+        filteredPokemons:[],
+        busca: ""
+    }
+    
+  },
+  created: function(){
+    //  o codigo é executado quando a pagina carrega
+
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(data =>{
+      
+
+      this.pokemons = data.data.results;
+      this.filteredPokemons = data.data.results;
+
+    })
+  },
+  components:{
+    pokemon
+  },
+
+  methods: {
+
+    buscar: function(){
+
+      this.filteredPokemons = this.pokemons;
+
+      if(this.busca == "" || this.busca == " "){
+          this.filteredPokemons = this.pokemons
+
+      }else{
+          this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+
+      }
+
+    }
+
+  },
+  computed:{
+    /*
+    resultadoBusca: function(){
+      if(this.busca == "" || this.busca == " "){
+        return this.pokemons
+
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+
+    }
+    */
   }
+ 
+
 }
 </script>
 
@@ -24,5 +92,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#busca_btn{
+  margin-top: 2%;
 }
 </style>
